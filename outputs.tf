f647ef0770671f55ce824aee8d155380e76e9546
BadeAjinkya <161459285+BadeAjinkya@users.noreply.github.com>
2024-08-08 15:43:48 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.web.ami
+  value = aws_instance.Ajinkya.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.web.arn
+  value = aws_instance.Ajinkya.arn
 }
